b1608d09ba6310a84bbbe088fc2dfd28dbf72ea4
dev634 <pablolola@live.fr>
2021-09-08 09:42:15 +0000

    add checking king in subdecks

--- Middlewares/checkImport.js
+++ Middlewares/checkImport.js
@@ -3,10 +3,20 @@ let middleware = multer.single('file');
 
 function checkImport(req, res, next){
   middleware(req,res, () => {
-      console.log(req.file.buffer.toString('utf-8'))
+     let file = JSON.parse(req.file.buffer.toString('utf-8'));
+
+     let eden = file.eden;
+     let holy_book = file.holy_book ;
+     let register = file.register;
+     let edenKingdoms = eden.map(elmt => elmt[1]);
+     let holyBookKingdoms = holy_book.map(elmt => elmt[1])
+     let registerKingdoms = register.map(elmt => elmt[1])
+     console.log("eden kingdoms: ", edenKingdoms);
+     console.log("holy book kingdoms: ", holyBookKingdoms);
+     console.log("register : ", registerKingdoms)
   });
 }
 
-module.exports = {
-    checkImport
-};
+module.exports ={
+    checkImport 
+}
---
+++ constantes/kingdomsByCategory.js
@@ -0,0 +1,9 @@
+const EDEN = [1,2,3, 11];
+const HOLYBOOK = [4, 5, 6, 9, 12];
+const REGISTER = [8];
+
+module.exports = {
+    EDEN,
+    HOLYBOOK,
+    REGISTER
+}
